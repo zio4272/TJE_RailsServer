e94ff6e6606c954786e49e30ed628630eec5b1c9
EC2 Default User <ec2-user@ip-172-31-11-157.ap-northeast-2.compute.internal>
2017-11-01 02:05:06 +0000

    회원가입

--- app/controllers/lm_controller.rb
+++ app/controllers/lm_controller.rb
@@ -1,6 +1,31 @@
 class LmController < ApplicationController
 
 
+    def register_user
+      # 회원가입 : 사용자에게 학생정보를 받아 회원으로 DB에 추가.
+
+      # 사용자 클래스 하나 생성
+      s = Student.new
+
+      # 학생 클래스에 들어있는 loginId 항목은
+      # 클라이언트가 id라고 꼬리표를 달아서 보내주는 항목으로 대입.
+      s.loginId = params[:id]
+      s.loginPw = params[:pw]
+      s.name = params[:name]
+      s.phone = params[:phone]
+
+      # 클래스 정보를 DB에 저장
+      s.save
+
+      # 동작을 하고 나면 클라이언트에게 결과를 전송
+      # 클라이언트는 JSON을 받는게 제일 편함. => JSON으로 가공해서 전송
+
+      # 수행 결과를 저장할 객체
+      resultJson = {"result" => TRUE, "message" => "user registered successfully"}
+      render json: resultJson
+
+    end
+
     def sign_up
       st = Student.new
       st.loginId = params[:login_id]
